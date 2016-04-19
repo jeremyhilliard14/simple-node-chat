7bfc7dcdd13e1add534259efde9ea1dad8c91bc2
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-04-19 17:07:07 -0400

    update port
    updated port to allow opening on heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var port = process.env.PORT || '8001';
 
 var server = http.createServer(function(request, response){
 	fs.readFile('index.html', 'UTF-8', function(error, data){
@@ -11,13 +12,19 @@ var server = http.createServer(function(request, response){
 });
 
 var io = require('socket.io').listen(server);
+var messageArray = [];
 
 io.sockets.on('connect', function(socket){
 	console.log(socket);
+	var date = new Date();
+	var time = date.toLocaleTimeString();
+	console.log(time)
 	socket.on('message_to_server', function(data){
-		io.sockets.emit('message_to_client', {message: data.message});
+		io.sockets.emit('message_to_client', {message: data.message, time: time});
+		messageArray.push(data.message);
 	})
 })
 
-server.listen(8001);
+
+server.listen(port);
 console.log('Server listening on port 8001');
